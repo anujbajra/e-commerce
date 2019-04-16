3f5068488509b4c841986069979598f39b41e014
anujbajra <anujbajra123@gmail.com>
2019-04-16 17:50:00 +0545

    Initial comment

---
+++ grails-app/controllers/e/commerce/statistics/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package e.commerce.statistics
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
